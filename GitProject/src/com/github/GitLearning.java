a8cb2e4d1898d4147a05e7d193aa62ec775cf3ab
kaniz <kanizbd100@gmail.com>
2020-11-30 21:40:33 -0500

    commit

---
+++ GitProject/src/com/github/GitLearning.java
@@ -0,0 +1,11 @@
+package com.github;
+
+public class GitLearning {
+
+	public static void main(String[] args) {
+		System.out.println("This is git learning project.");
+		System.out.println("commit and push .");
+
+	}
+
+}
