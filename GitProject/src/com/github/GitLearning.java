f8fa54c2f9e4e49cfbc34456da2f1db40043f40e
moh <moh@192.168.1.13>
2020-12-03 00:21:07 -0500

    Mukit just edit GitProject

--- GitProject/src/com/github/GitLearning.java
+++ GitProject/src/com/github/GitLearning.java
@@ -6,8 +6,9 @@ public class GitLearning {
 		System.out.println("This is git learning project.");
 		System.out.println("commit and push .");
 		System.out.println("Rony");
-		System.out.println("Mukit Edit");
+		System.out.println("Mukit Edit on December 2nd ");
 		System.out.println("K M Tarek");
+		
 
 	}
 
